0db7451d777cd9247b11a4962cbcaca13cdc67e5
Greg Hope <greg.hope@outlook.com>
2024-10-28 17:50:56 +0000

    Decoder load states

--- src/decoder.v
+++ src/decoder.v
@@ -20,6 +20,9 @@ module decoder(
   output reg s_store,
   output reg s_read
 );
+  localparam [3:0] PC = 0;
+  //localparam [3:0] SP = 1;
+
   // States
   localparam IDLE = 0;
   localparam FETCH = 1;
@@ -36,24 +39,94 @@ module decoder(
     else current_state <= next_state;
   end
 
+  // Load states
+  localparam LOAD_IDLE = 0; // Rn out
+  localparam LOAD_INDEXPC = 1; // pc+ -> MAR
+  localparam LOAD_INDEXREG = 2; // Rn+MDR -> MAR
+  localparam LOAD_INDIRECT = 3; // Rn -> MAR
+  localparam LOAD_INCREMENT = 4; // Rn+ -> MAR
+  localparam LOAD_DONE = 5; // MDR out
+
+  reg [2:0] current_load_state;
+  reg [2:0] next_load_state;
+
+  always @(posedge clk) begin
+    if (srst) current_load_state <= LOAD_IDLE;
+    else current_load_state <= next_load_state;
+  end
+
+  wire load_done;
+  assign load_done =
+      (current_load_state == LOAD_DONE)
+    | (current_load_state == LOAD_IDLE);
+
   // Instruction register
-  /* verilator lint_off UNUSEDSIGNAL */
-  reg [15:0] instruction;
-  /* verilator lint_on UNUSEDSIGNAL */
-  always @(posedge clk) if (current_state == DECODE) instruction <= data_in;
+  reg  [15:0] instruction_reg;
+  wire [15:0] instruction;
+
+  assign instruction = current_state == DECODE ? data_in : instruction_reg;
+  always @(posedge clk) if (current_state == DECODE) instruction_reg <= data_in;
 
   // State transitions
   always @(*) begin
     next_state = ERROR;
+
     case (current_state)
       IDLE: next_state = FETCH;
       FETCH: next_state = DECODE;
       DECODE: begin
-        // TODO: handle other instructions
-        next_state = MOVSOURCE;
+        if (instruction[15:12] == 4'h4)
+          next_state = MOVSOURCE;
+        else
+          next_state = IDLE;
       end
-      MOVSOURCE: next_state = MOVDEST;
+      MOVSOURCE: next_state = load_done ? MOVDEST : MOVSOURCE;
       MOVDEST: next_state = FETCH;
+    endcase
+  end
+
+  // Load state transitons
+  reg       do_load;
+  reg [3:0] do_load_regno;
+  reg [1:0] do_load_as;
+
+  always @(*) begin
+    next_load_state = LOAD_IDLE;
+
+    if (do_load) begin
+      if ((do_load_regno) == 2 & do_load_as[1])
+        next_load_state = LOAD_IDLE;
+      else if (do_load_regno == 3)
+        next_load_state = LOAD_IDLE;
+      else begin
+        case (do_load_as)
+          0: next_load_state = LOAD_IDLE;
+          1: next_load_state = LOAD_INDEXPC;
+          2: next_load_state = LOAD_INDIRECT;
+          3: next_load_state = LOAD_INCREMENT;
+        endcase
+      end
+    end
+
+    case (current_load_state)
+      LOAD_INDEXPC:   next_load_state = LOAD_INDEXREG;
+      LOAD_INDEXREG:  next_load_state = LOAD_DONE;
+      LOAD_INDIRECT:  next_load_state = LOAD_DONE;
+      LOAD_INCREMENT: next_load_state = LOAD_DONE;
+    endcase
+  end
+
+  always @(*) begin
+    do_load       = 0;
+    do_load_regno = 0;
+    do_load_as    = 0;
+
+    case (next_state)
+      MOVSOURCE: begin
+        do_load       = current_state != next_state;
+        do_load_regno = instruction[11:8];
+        do_load_as    = instruction [5:4];
+      end
     endcase
   end
 
@@ -71,18 +144,17 @@ module decoder(
 
     case (current_state)
       FETCH: begin
-        regno   = 0; // PC
-        As      = 2'b11; // @Rn+
-        reg_inc = 1;
+        regno     = PC;
+        reg_inc   = 1;
       end
       DECODE: begin
-        ram_read = 1;
+        ram_read  = 1;
       end
       MOVSOURCE: begin
-        regno    = instruction[11:8];
-        As       = instruction [5:4];
-        bytemode = instruction   [6];
-        s_store  = 1;
+        regno     = instruction[11:8];
+        As        = instruction [5:4];
+        bytemode  = instruction   [6];
+        s_store   = 1;
       end
       MOVDEST: begin
         regno     = instruction [3:0];
@@ -92,6 +164,29 @@ module decoder(
         s_read    = 1;
       end
     endcase
-  end
 
+    case (current_load_state)
+      LOAD_INDEXPC: begin
+        regno    = 0;
+        reg_inc  = 1;
+      end
+      LOAD_INDEXREG: begin
+        regno    = do_load_regno;
+        As       = do_load_as;
+        ram_read = 1;
+      end
+      LOAD_INDIRECT: begin
+        regno    = do_load_regno;
+        As       = do_load_as;
+      end
+      LOAD_INCREMENT: begin
+        regno    = do_load_regno;
+        As       = do_load_as;
+        reg_inc  = 1;
+      end
+      LOAD_DONE: begin
+        ram_read = 1;
+      end
+    endcase
+  end
 endmodule
--- test/decoder_tb.v
+++ test/decoder_tb.v
@@ -1,15 +1,21 @@
 module decoder_tb();
-  
+
   reg clk;
   reg srst;
 
-  wire [15:0] rom [0:7];
+  wire [15:0] rom [0:15];
   assign rom[0] = 16'h4315;  // mov #1, r5
   assign rom[1] = 16'h4326;  // mov #2, r6
   assign rom[2] = 16'h4337;  // mov #-1, r7
   assign rom[3] = 16'h4378;  // mov.b #ff, r8
   assign rom[4] = 16'h4749;  // mov.b r7, r9
-  assign rom[5] = 16'h4300;  // br #0
+  assign rom[5] = 16'h4634;  // mov @r6+, r4
+  assign rom[6] = 16'h4634;  // mov @r6+, r4
+  assign rom[7] = 16'h4035;  // mov #x1234, r5
+  assign rom[8] = 16'h1234;
+  assign rom[9] = 16'h4216;  // mov &10, r6
+  assign rom[10] = 16'h000a;
+  assign rom[11] = 16'h4300;  // br #0
 
   initial begin
     clk = 0;
@@ -19,7 +25,7 @@ module decoder_tb();
     $dumpvars(0, decoder_tb);
 
     #4 srst = 0;
-    #50 $finish;
+    #100 $finish;
   end
   initial forever #1 clk=~clk;
 
@@ -38,10 +44,10 @@ module decoder_tb();
   reg [15:0] s;
   /* verilator lint_on UNUSEDSIGNAL */
 
-  always @(posedge clk) if (s_store) s <= reg_bus;
+  always @(posedge clk) if (s_store) s <= ram_read ? data_bus : reg_bus;
 
-  reg [2:0] ram_address;
-  always @(posedge clk) ram_address <= reg_bus[3:1];
+  reg [3:0] ram_address;
+  always @(posedge clk) ram_address <= reg_bus[4:1] + (ram_read ? data_bus[4:1] : 0);
 
   always @(*) begin
     if (ram_read) data_bus = rom[ram_address];
@@ -51,7 +57,7 @@ module decoder_tb();
 
   decoder decoder(
     .clk(clk), .srst(srst), .data_in(data_bus),
-    .bytemode(bytemode), 
+    .bytemode(bytemode),
     .As(As), .regno(regno), .reg_store(reg_store), .reg_inc(reg_inc),
     .s_store(s_store), .s_read(s_read),
     .ram_store(ram_store), .ram_read(ram_read)
